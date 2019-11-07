7aee7057f6831b6669294748467556831528a79c
Marino Soro <soromarino@gmail.com>
2019-11-07 21:18:00 +0100

    Added lodash as a plugin to nuxt, extended examples

--- Project/Frontend/nuxt/Groupfinder/pages/index.vue
+++ Project/Frontend/nuxt/Groupfinder/pages/index.vue
@@ -1,17 +1,60 @@
 <template>
   <div class="container">
-    <div>
-      <NameCard :user="{first_name: 'Liese', last_name: 'Bekkers', email: 'liese.bekkers@student.uhasselt.be'}" />
+    <h1 class="mt-5">BI Studenten</h1>
+    <div class="row">
+      <div v-for="user in biStudents" :key="user.email" class="col-4">
+        <NameCard :user="user" />
+      </div>
+    </div>
+
+    <h1 class="mt-5">IT Studenten</h1>
+    <div class="row">
+      <div v-for="user in itStudents" :key="user.email" class="col-4">
+        <NameCard :user="user" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import NameCard from '~/components/NameCard/NameCard.vue'
+import NameCard from '~/components/NameCard/NameCard'
 
 export default {
   components: {
     NameCard
+  },
+  data () {
+    return {
+      users: [
+        {
+          first_name: 'Liese',
+          last_name: 'Bekkers',
+          email: 'liese.bekkers@student.uhasselt.be'
+        },
+        {
+          first_name: 'Marino',
+          last_name: 'Soro',
+          email: 'marino.soro@student.uhasselt.be'
+        },
+        {
+          first_name: 'Lennert',
+          last_name: 'Geebelen',
+          email: 'lennert.geebelen@student.uhasselt.be'
+        }
+      ]
+    }
+  },
+  computed: {
+    biStudents () {
+      return this.$_.filter(this.users, (o) => {
+        return o.first_name === 'Marino'
+      })
+    },
+    itStudents () {
+      return this.$_.filter(this.users, (o) => {
+        return o.first_name !== 'Marino'
+      })
+    }
   }
 }
 </script>
@@ -19,6 +62,5 @@ export default {
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
 }
 </style>
