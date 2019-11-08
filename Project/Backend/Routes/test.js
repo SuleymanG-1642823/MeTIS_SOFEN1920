ca9c97173570088dea811344827167b0d9c9176a
Marino Soro <soromarino+github@gmail.com>
2019-11-08 11:07:53 +0100

    Added: middleware for test router

--- Project/Backend/Routes/test.js
+++ Project/Backend/Routes/test.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+/////////////////////////////////////////////////////////////////////////////////////
+// middleware that is specific to this router
+/////////////////////////////////////////////////////////////////////////////////////
+router.use(function testMiddleware (req, res, next) {
+    console.log(`Test middleware is triggered`);
+    next()
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+// define routes
+/////////////////////////////////////////////////////////////////////////////////////
 router.get('/', (req, res) => {
     res.status(200).send('This is a test route.')
 });
@@ -8,6 +19,6 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     // Normally, data from the payload would be handled here.
     res.status(200).send('POST request received.')
-})
+});
 
 module.exports = router;
