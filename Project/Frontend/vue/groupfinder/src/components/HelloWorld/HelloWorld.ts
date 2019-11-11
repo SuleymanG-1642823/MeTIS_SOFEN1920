b27e0fd86885312bdef7545adadf08fea49981aa
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-11-11 18:05:24 +0100

    Improved: docker for frontend&backend
    TODO: run nuxt project in docker container in development mode instead of production mode

--- Project/Frontend/vue/groupfinder/src/components/HelloWorld/HelloWorld.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import WithRender from './HelloWorld.html';
-require('./HelloWorld.css');
-
-@ WithRender
-@ Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
