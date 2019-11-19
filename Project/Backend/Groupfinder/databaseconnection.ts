0d3acaefa9b45fa158097a78eba38fda7ca16fbf
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-11-19 18:31:15 +0100

    Fixed: databaseconnection from groupfinderServerContainer

--- Project/Backend/Groupfinder/databaseconnection.ts
+++ Project/Backend/Groupfinder/databaseconnection.ts
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost', /* default: localhost */
+    host: 'db', /* default: localhost */
     port: '3306', /* default: 3306 */
     user: 'finder',
     password: '9=&c0dL$_N+9+_}pmgx]4JE*',
