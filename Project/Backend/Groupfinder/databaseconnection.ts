d45ac0aeae4aab7f825407869b7f919e7e3c5187
Lennert Geebelen <lennert.geebelen@student.uhasselt.be>
2019-11-19 02:56:29 +0100

    Added: connection information in backend file

--- Project/Backend/Groupfinder/databaseconnection.ts
+++ Project/Backend/Groupfinder/databaseconnection.ts
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'TODO', /* default: localhost */
-    port: 'TODO', /* default: 3306 */
-    user: 'TODO',
-    password: 'TODO',
-    database: 'TODO'
+    host: 'localhost', /* default: localhost */
+    port: '3306', /* default: 3306 */
+    user: 'finder',
+    password: '9=&c0dL$_N+9+_}pmgx]4JE*',
+    database: 'groupfinder'
 });
 
 module.exports = connection;
