e40abe43b7f09b6fa8beeb288cd06e70b35522c8
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-12-03 20:03:47 +0100

    Added: test data

--- Project/Database/sql-scripts/03_InsertData.sql
+++ Project/Database/sql-scripts/03_InsertData.sql
@@ -1,3 +1,13 @@
 USE groupfinder;
-INSERT INTO user (first_name, last_name, mail)
-VALUES ('Lennert', 'Geebelen', 'lennert.geebelen@student.uhasselt.be');
+INSERT INTO user (id, first_name, last_name, mail)
+VALUES (1, 'Lennert', 'Geebelen', 'lennert.geebelen@student.uhasselt.be');
+INSERT INTO user (id, first_name, last_name, mail)
+VALUES (2, 'Dore', 'Staquet', 'dore.staquet@student.uhasselt.be');
+INSERT INTO project (id, creator_id, name, status)
+VALUES (1, 2, 'Price Spotter', 1);
+INSERT INTO profile (id, name, project_id)
+VALUES (1, 'Android app developer', 1);
+INSERT INTO profile_skill
+VALUES (1, 'Android Studio', 1, 1);
+INSERT INTO category 
+VALUES (1, 'Mobile');
