1b3ccf22869f580917c86f0c4396a0083c96910f
Suleyman <suleyman.guney@student.uhasselt.be>
2019-12-18 19:23:03 +0100

    More testing

--- Project/Backend/Groupfinder/classes/matching_algorithm_tests/ProjectsToUserMatcherTest.ts
+++ Project/Backend/Groupfinder/classes/matching_algorithm_tests/ProjectsToUserMatcherTest.ts
@@ -123,7 +123,10 @@ export default class ProjectsToUserMatcherTest{
         await addNewProfile(8000, 'Profile A', 7000); // testProject1 (7000)
         await addNewProfile(8001, 'Profile B', 7000);
         await addNewProfile(8002, 'Profile C', 7000);
-        
+        await addNewProfile(8003, 'Profile weight1', 7000);
+        await addNewProfile(8004, 'Profile experience1', 7000);
+        await addNewProfile(8005, 'Profile matching perc all', 7000);
+
         // profile skills (profile_id, skill_name, skill_experience, weight)
         await addNewProfileSkill(8000, 'SKILL ALPHA', 1, 1);  // Profile A
         await addNewProfileSkill(8000, 'SKILL BETA', 1, 1);
@@ -135,11 +138,27 @@ export default class ProjectsToUserMatcherTest{
         
         await addNewProfileSkill(8002, 'SKILL GIGA', 1, 1);  // Profile C
         await addNewProfileSkill(8002, 'SKILL MEGA', 1, 1);
+
+        await addNewProfileSkill(8003, 'SKILL ALPHA', 1, 4);  // Profile Weight1
+        await addNewProfileSkill(8003, 'SKILL KILO', 1, 1);
+        await addNewProfileSkill(8003, 'SKILL TETA', 1, 2);
+
+        await addNewProfileSkill(8004, 'SKILL EPSILON', 7, 1);  // Profile experience1
+        await addNewProfileSkill(8004, 'SKILL SIGMA', 5, 1);
+        await addNewProfileSkill(8004, 'SKILL PI', 5, 1);
         
+        await addNewProfileSkill(8005, 'SKILL LAMBDA', 7, 5);  // Profile matching perc all
+        await addNewProfileSkill(8005, 'SKILL ALPHA', 2, 2); 
+        await addNewProfileSkill(8005, 'SKILL TETA', 3, 4); 
+        await addNewProfileSkill(8005, 'SKILL SIGMA', 3, 3); 
+
         // user Skills
         await addNewUserSkill(6001, 'SKILL ALPHA', 1);  // testUser2
         await addNewUserSkill(6001, 'SKILL BETA', 1);
         await addNewUserSkill(6001, 'SKILL TETA', 1);   
+        await addNewUserSkill(6001, 'SKILL EPSILON', 5);   
+        await addNewUserSkill(6001, 'SKILL SIGMA', 3);   
+        await addNewUserSkill(6001, 'SKILL PI', 5);   
     }
 
     /**
@@ -191,6 +210,29 @@ export default class ProjectsToUserMatcherTest{
         }
     }
 
+    /**
+     * Checks whether given list of matches is sorted
+     * @param matches: array of project matches
+     */
+    private static isSorted(projectMatches: Array<ProjectMatch>): boolean{
+        for (let i = 0; i < (projectMatches.length-1); i++){ // matches.length -1 because we need to ensure there is a next elem
+            // projects must be sorted on descending order according on their biggest matching percentage
+            if (projectMatches[i].matches[0].matchingPercentile < projectMatches[i+1].matches[0].matchingPercentile){
+                return false;
+            }
+
+            const currentProjectMatches = projectMatches[i].matches;
+            for (let j = 0; j < (currentProjectMatches.length - 1); j++){
+                // profiles must be sorted on descending order
+                if (currentProjectMatches[j].matchingPercentile < currentProjectMatches[j+1].matchingPercentile){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
    public static executeTests(): Promise<void>{
         return new Promise(async (resolve: any, reject: any) => {
             console.log('##################################################################################################')
@@ -205,22 +247,44 @@ export default class ProjectsToUserMatcherTest{
                             let matchingResult: Array<ProjectMatch> = await ProjectsToUserMatcher.getMatchingProjects(6001, db_conn);
                             let resultingMsg: string = '';
 
-                            // There must be results
+                            // there must be results
                             if (matchingResult.length === 0){
-                                resultingMsg = '[1.0] FAILED: no matches returned';
+                                resultingMsg = 'FAILED: no matches returned';
                             }
                             else{
-                                // Check matching % with Profiles 
+                                // check sorting order
+                                if (this.isSorted(matchingResult)){
+                                    resultingMsg += '[1.0] SUCCESS\n';
+                                }
+                                else{
+                                    resultingMsg += '[1.0] FAILED\n';
+                                }
+
+                                // check matching % with Profiles 
                                 const profileMatches = matchingResult[0].matches;
-                                
+
                                 // 0% because testUser2 has none of the required skills for profileC
                                 resultingMsg += this.equalsHelper(profileMatches[0].matchingPercentile, 100, '[1.1] ');
 
                                 // must be 67% (rounded up) because the testUser2 has 2 of the required skills for profileB
-                                resultingMsg += this.equalsHelper(profileMatches[1].matchingPercentile, 67, '[1.2] ');
+                                resultingMsg += this.equalsHelper(profileMatches[3].matchingPercentile, 67, '[1.2] ');
 
                                 // 0% because testUser2 has none of the required skills for profileC
                                 //resultingMsg += this.equalsHelper(profileMatches[2].matchingPercentile, 0, '[1.3] ');
+
+                                // test weights, 3 of the 3 skills match, weights differ: matching weights are 2 and 4, skill with weight 1 doesn't match
+                                // result must be: (4+2)/(4+2+1) = 6/7 = 86% (round up)
+                                resultingMsg += this.equalsHelper(profileMatches[1].matchingPercentile, 86, '[1.3] ');
+
+                                // matching percentage must be proportional to the ration of possessed skill/required skill
+                                // profile experience 1 matching percentage: (5/7 + 3/5 + 5/5) / 3  = 78%
+                                resultingMsg += this.equalsHelper(profileMatches[2].matchingPercentile, 78, '[1.4] ');
+
+                                // all at once: Profile matching perc all
+                                // (1 + (1/3)*4 + 3) / (5+2+4+3) = (4 + 4/3) / 14 = 39%
+                                resultingMsg += this.equalsHelper(profileMatches[4].matchingPercentile, 39, '[1.5] ');
+                                
+                                // If all tests pass that means all expected profiles came in the expected indexes which means the correct profiles and projects are returned
                             }
 
                             return resultingMsg;
