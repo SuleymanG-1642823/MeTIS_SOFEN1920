e146a23ce63c94d78734cd6ef4ecc995f1c70c50
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-12-24 19:26:41 +0100

    Added: /preferences routes (GET + POST + DELETE)

--- Project/Backend/Groupfinder/routes/preferences/preferences.ts
+++ Project/Backend/Groupfinder/routes/preferences/preferences.ts
@@ -1,17 +1,68 @@
 import express from 'express';
 const router = express.Router();
 import Category from '../../types/category';
-import { Preferences } from './preferences_methods';
+import { PreferenceController } from './preferences_methods';
+
+let controller: PreferenceController;
 
 /**
  * Middleware that is specific to this router
  */
 router.use((req: any, res: any, next: Function) => {
+    controller = new PreferenceController();
     next()
 });
 
 /////////////////////////////////////////////////////////////////////////////////////
 // Define routes
 /////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Get all categories preferred by a user from the database.
+ * url param type must be a boolean (true for preferred categories, false for non-preferred)
+ */
+router.get('/:user_id/:type', async (req: any, res: any) => {
+    try {
+        const userID: number = parseInt(req.params.user_id);
+        const type: boolean = req.params.type;
+        const categories: Category[] = await controller.getPreferences(userID, type);
+        res.status(200).json(categories);
+    } catch (err) {
+        const code: number = parseInt(err);
+        res.status(code).send("Error while fetching preferred categories from the database.")
+    }
+})
+
+/**
+ * Insert categories (non-)preferred by a user into the database.
+ * url param type must be a boolean (true for preferred categories, false for non-preferred)
+ */
+router.post('/:user_id/:type', async (req: any, res: any) => {
+    try{
+        const userID: number = parseInt(req.params.user_id);
+        const type: boolean = req.params.type;
+        const categories: Category[] = req.body.categories;
+        await controller.addPreferences(userID, categories, type);
+        res.status(200).send('Successfully added preferred categories into the database.')
+    } catch (err) {
+        const code: number = parseInt(err);
+        res.status(code).send("Error while inserting preferred categories into the database.")
+    }
+})
+
+/**
+ * Delete preferred categories from the database.
+ */
+router.delete('/:user_id', async (req: any, res: any) => {
+    try{
+        const userID: number = parseInt(req.params.user_id);
+        const categories: Category[] = req.body.categories;
+        await controller.deletePreferences(userID, categories);
+        res.status(200).send('Successfully deleted preferred categories from the database.');
+    } catch (err) {
+        const code: number = parseInt(err);
+        res.status(code).send("Error while deleting preferred categories from the database.");
+    }
+})
 
 module.exports = router;
--- Project/Backend/Groupfinder/routes/preferences/preferences_methods.ts
+++ Project/Backend/Groupfinder/routes/preferences/preferences_methods.ts
@@ -1,7 +1,16 @@
 const db_conn = require('../../databaseconnection');
 import Category from '../../types/category';
 
-export class Preferences {
+/**
+ * Manage all actions with the preference (and category) table in the database.
+ * This class can get, insert, change and delete categories preferred by a user.
+ */
+export class PreferenceController {
+    /**
+     * Get all preferred categories by a user.
+     * @param userID the id of the user.
+     * @param type true for fetching preferred categories, false for fetching non-preferred categories
+     */
     public getPreferences(userID: number, type: boolean): Promise<Category[]> {
         return new Promise(
             (resolve, reject) => {
@@ -27,6 +36,88 @@ export class Preferences {
                         resolve(categories);
                     }
                 });
+            }
+        );
+    }
+
+
+    /**
+     * Insert categories (non-)preferred by a user into the database.
+     * @param userID the id of the user.
+     * @param categories categories to be inserted.
+     * @param type true for fetching preferred categories, false for fetching non-preferred categories.
+     */
+    public addPreferences(userID: number, categories: Category[], type: boolean): Promise<void> {
+        return new Promise(
+            async (resolve, reject) => {
+                for (let i = 0; i < categories.length; i++) {
+                    await this.addPreference(userID, categories[i], type);
+                }
+            }
+        );
+    }
+
+
+    /**
+     * Delete preferred categories from the database.
+     * @param userID id of the user.
+     * @param categories categories to be deleted as preferred category.
+     * @param type true for deleting preferred category, false for deleting non-preferred category.
+     */
+    public deletePreferences(userID: number, categories: Category[]): Promise<void> {
+        return new Promise(
+            async (resolve, reject) => {
+                for (let i = 0; i < categories.length; i++){
+                    await this.deletePreference(userID, categories[i]);
+                }
+            }
+        );
+    }
+
+
+    /**
+     * Insert category (non-)preferred by a user into the database.
+     * @param userID the id of the user.
+     * @param category (non-)preferred category to be inserted.
+     * @param type true for inserting preferred category, false for inserting non-preferred category.
+     */
+    private addPreference(userID: number, category: Category, type: boolean): Promise<void> {
+        return new Promise(
+            (resolve, reject) => {
+                const query: string = "INSERT INTO preference VALUES (?,?,?);";
+                const params: any[] = [userID, category.id, type];
+                db_conn.query(query, params, (err: any, rows: any) => {
+                    if (err) {
+                        console.log(err);
+                        reject('500')
+                    } else {
+                        reject();
+                    }
+                })
+            }
+        );
+    }
+
+
+    /**
+     * Delete a user's category preference from the database.
+     * @param userID id of the user.
+     * @param category category to be deleted as preferred category.
+     * @param type true for deleting preferred category, false for deleting non-preferred category.
+     */
+    private deletePreference(userID: number, category: Category): Promise<void> {
+        return new Promise(
+            (resolve, reject) => {
+                const query: string = "DELETE FROM preference WHERE user_id=? AND category_id=?;";
+                const params: any[] = [userID, category.id];
+                db_conn.query(query, params, (err: any, rows: any) => {
+                    if (err) {
+                        console.log(err);
+                        reject('500');
+                    } else {
+                        resolve();
+                    }
+                })
             }
         );
     }
