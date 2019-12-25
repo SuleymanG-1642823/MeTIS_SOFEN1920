080538042cc11dd2e81ccac5870f8019dee0c51e
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-12-25 13:00:46 +0100

    Added: tests for /preferences routes

---
+++ Project/Backend/Groupfinder/__tests__/preferences.js
@@ -0,0 +1,81 @@
+import app from '../app';
+import http from 'http';
+import supertest from 'supertest';
+import 'babel-polyfill';
+
+describe("TESTING ALL PREFERENCES ROUTES", () => {
+    beforeAll((done) => {
+        var test_server = http.createServer(app);
+        test_server.listen(done);
+        request = supertest(test_server);
+    });
+    afterAll((done) => {
+        test_server.close(done);
+    });
+    describe('POST /preferences/:user_id/:type', () => {
+        it("Should insert a new preference into the database", (done) => {
+           var body = {
+               categories: [
+                   {
+                        id: 1,
+                        name: 'Website',
+                        subcategory: null
+                   },
+                   {
+                       id: 2,
+                       name: 'Game Development',
+                       subcategory: null
+                   }
+               ]
+           }
+           const userID = 1;
+           const type = true;
+           request.post(`/preferences/${userID}/${type}`)
+           .send(body)
+           .end((err, res) => {
+               if (err) return done(err);
+               expect(res.status).toBe(200);
+               done();
+           })
+        });
+    })
+    describe('GET /preferences/:user_id/:type', () => {
+        it("Should get all categories preferred by a user from the database.", (done) => {
+           const userID = 1;
+           const type = true;
+           request.get(`/preferences/${userID}/${type}`)
+           .end((err, res) => {
+               if (err) return done(err);
+               expect(res.status).toBe(200);
+               expect(typeof(res.body)).toBe('object');
+               done();
+           })
+        })
+    })
+    describe('DELETE /preferences/:user_id', () => {
+        it("Should delete preferred categories from the database.", (done) => {
+            var body = {
+                categories: [
+                    {
+                         id: 1,
+                         name: 'Website',
+                         subcategory: null
+                    },
+                    {
+                        id: 2,
+                        name: 'Game Development',
+                        subcategory: null
+                    }
+                ]
+            }
+            const userID = 1;
+            request.delete(`/preferences/${userID}`)
+            .send(body)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.status).toBe(200);
+                done();
+            })
+        })
+    })
+})
