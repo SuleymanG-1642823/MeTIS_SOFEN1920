b7a133fed37a2da5a9fb64c78fadce382b2ab176
Dore Staquet <dore.staquet@student.uhasselt.be>
2019-12-30 14:53:56 +0100

    Updated save and add profile buttons so layout looks better

--- Project/Frontend/Groupfinder/pages/projectCreationForm/index.vue
+++ Project/Frontend/Groupfinder/pages/projectCreationForm/index.vue
@@ -5,10 +5,22 @@
       <b-modal v-model="modalShow" title="Form not correctly filled in!">
         <p v-if="categoriesNotValidated == 'true'">Select one or more categories</p>
       </b-modal>
-      <b-button type="submit" variant="success">Submit Project</b-button>
+      <b-container fluid>
+        <b-row align-h="center" align-content="center">
+          <b-button class="submit-btn" type="submit" variant="success">Submit Project</b-button>
+        </b-row>
+      </b-container>
     </b-form>
   </div>
 </template>
 
 <script lang="ts" src="./projectCreationForm.ts">
 </script>
+
+<style scoped>
+
+.submit-btn{
+  width: 15%;
+}
+
+</style>
