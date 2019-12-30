f5a4329873a4d21e6fb4964dafef02de649ec674
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2019-12-30 01:33:05 +0100

    Improved: userPage layout

--- Project/Frontend/Groupfinder/components/ProjectCardWithReviews/ProjectCardWithReviews.vue
+++ Project/Frontend/Groupfinder/components/ProjectCardWithReviews/ProjectCardWithReviews.vue
@@ -1,9 +1,8 @@
 <template>
     <b-card v-if="this.project" :title="this.project.name" :sub-title="creatorName">
         <b-card-text>
-            <p style="float: right;">Average rating: <star-rating v-model="this.avgRating" :star-size="20" :increment="0.5" :max-rating="5" :read-only="true"></star-rating></p>
+            <p style="float: right;">Average rating: <star-rating v-model="this.avgRating" :star-size="20" :increment="0.5" :max-rating="5" :read-only="true"></star-rating>({{ nRatings }} in total)</p>
             <p>{{ this.project.pitch }}</p>
-            <p>Status: {{ this.project.status }}</p>
             <b-button v-if="this.nRatings > 0" v-b-toggle="this.project.id.toString()" @click="changeButtonText()">{{ this.buttonText }}</b-button>
             <b-collapse v-if="this.nRatings > 0" :id="this.project.id.toString()">
                 <b-card v-for="review in this.reviews" v-bind:key="review.id" :sub-title="createFullName(review.writer_first_name, review.writer_last_name)">
